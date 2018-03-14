c6cda56f0d70d0a8820631a358a7c5907ee94871
Ryan Weddle <weddle@gmail.com>
2018-03-14 10:38:30 -0700

    Create DevConstants.java

---
+++ app/src/main/java/com/ryanweddle/simplewrappertest/DevConstants.java
@@ -0,0 +1,12 @@
+
+package com.ryanweddle.simplewrappertest;
+
+/**
+ * Created by ryweddle on 3/11/18.
+ */
+
+public class DevConstants {
+
+    public static final String EXAMPLE_JWT = "DEFAULT_JWT_TOKEN";
+    public static final String EXAMPLE_SPARKID = "sparkid@domain.com";
+}
